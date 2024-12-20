a725f9aadc13805508c208c3f9bab60a54f29d24
HeinWeb <h.sithu319@gmail.com>
2024-12-20 22:21:57 +0630

    second commit 'complete delete and edit' stage

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ const result = document.querySelector(".result");
 // there is no value in toDo make it empty arry to prevent duplicating of local storage.
 let toDo = JSON.parse(localStorage.getItem("to-do-list")) || [];
 
+let isUserEdit = false; //to check user click edit or not;
+let userEditId; //to know which item user want to edit;
+
 function toShowTasks() {
+  inputBox.focus();
+  result.innerHTML = ""; //To prevent duplicate result output
   toDo.forEach((value, index) => {
+    let alreadyDone = value.status == "done" ? "done" : "";
     result.innerHTML += `
         <div class="forComplete">
-            <div id=${index} class="userValue">
+            <div id=${index} class="userValue ${alreadyDone}">
                 <p>${value.task}</p>
             </div>
             <div>
-                <span class="edit">🔄️</span>
-                <span class="delete">❌</span>
+                <span class="edit" id="${index}">🔄️</span>
+                <span class="delete" id="${index}">❌</span>
             </div>
         </div>
         `;
@@ -33,24 +39,49 @@ function toShowTasks() {
       }
     });
   }
+  // For delete button
+  const deleteBtn = document.querySelectorAll(".delete");
+  deleteBtn.forEach((k) => {
+    k.addEventListener("click", () => {
+      let userWantToDelete = k.id;
+      toDo.splice(userWantToDelete, 1);
+      localStorage.setItem("to-do-list", JSON.stringify(toDo));
+      toShowTasks();
+    });
+  });
+  // For Edit button
+  const editBtn = document.querySelectorAll(".edit");
+  for (let k of editBtn) {
+    let userWantToEdit = k.id;
+    k.addEventListener("click", () => {
+      inputBox.value = toDo[userWantToEdit].task;
+      inputBox.select();
+      userEditId = userWantToEdit;
+      isUserEdit = true;
+      toShowTasks();
+    });
+  }
+  console.log(userEditId);
 }
 
 const toDoList = () => {
   let userInput = inputBox.value;
-  // there is no value it's doesn't work
-  if (userInput) {
-    let userTasks = { task: userInput, status: "pending" };
-    toDo.push(userTasks);
-    localStorage.setItem("to-do-list", JSON.stringify(toDo));
-    inputBox.value = "";
-    toShowTasks();
+  if (isUserEdit) {
+    toDo[userEditId].task = userInput;
+  } else {
+    // there is no value it's doesn't work
+    if (userInput) {
+      let userTasks = { task: userInput, status: "pending" };
+      toDo.push(userTasks);
+    }
   }
+  inputBox.value = "";
+  localStorage.setItem("to-do-list", JSON.stringify(toDo));
+  toShowTasks();
 };
 
 addBtn.addEventListener("click", toDoList);
 inputBox.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    toDoList();
-  }
+  if (e.key === "Enter") toDoList();
 });
 toShowTasks();
